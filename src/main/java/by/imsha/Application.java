c969f97091fd6170eefa03255c4801d1290eecb9
childRon <andrei.misan@gmail.com>
2016-10-03 21:56:52 +0300

    add jsondocs pure

--- src/main/java/by/imsha/Application.java
+++ src/main/java/by/imsha/Application.java
@@ -5,6 +5,7 @@ import by.imsha.repository.factory.QuerableMongoRepositoryFactoryBean;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mangofactory.swagger.plugin.EnableSwagger;
+import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -27,6 +28,7 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 @EnableMongoRepositories(
         repositoryFactoryBeanClass = QuerableMongoRepositoryFactoryBean.class
 )
+@EnableJSONDoc
 @EnableSwagger // auto generation of API docs
 @PropertySources({
         @PropertySource("classpath:application.properties"),
