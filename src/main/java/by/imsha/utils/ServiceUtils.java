50213edcb601190f119038e7eea92c2974ef8cf8
childRon <andrei.misan@gmail.com>
2018-05-15 02:36:57 +0300

    add feature for actual mess

--- src/main/java/by/imsha/utils/ServiceUtils.java
+++ src/main/java/by/imsha/utils/ServiceUtils.java
@@ -1,13 +1,18 @@
 package by.imsha.utils;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * @author Alena Misan
  */
 public class ServiceUtils {
+
+    private static String dateFormat = "dd-MM-yyyy";
 
     public static String[] parseSortValue(String sort){
         if(StringUtils.isBlank(sort)){
@@ -29,6 +34,25 @@ public class ServiceUtils {
 
     public static LocalDateTime timestampToLocalDate(long timestamp, ZoneId zoneId){
         return ZonedDateTime.ofInstant ( Instant.ofEpochSecond ( timestamp ) , zoneId ).toLocalDateTime();
+    }
+
+    public static long dateToUTCTimestamp(String day) throws DateTimeParseException{
+        LocalDate date = null;
+        if(day != null){
+            try{
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormat);
+                date = LocalDate.parse(day, formatter);
+            }catch (DateTimeParseException ex){
+                throw new DateTimeParseException(String.format("Date format is incorrect. Date - %s,format - %s ", day, dateFormat), ex.getParsedString(), ex.getErrorIndex());
+            }
+        }
+
+        return date.atStartOfDay().toEpochSecond(ZoneOffset.UTC);
+    }
+
+    public static ZonedDateTime localDateTimeToZoneDateTime(LocalDateTime localDateTime, ZoneId fromZone, ZoneId toZone) {
+        ZonedDateTime date = ZonedDateTime.of(localDateTime, fromZone);
+        return date.withZoneSameInstant(toZone);
     }
 
 
